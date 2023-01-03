09545c2784b94d50e6c714d46bcff5d83b0811c9
LEIYAO123 <lei@rushable.io>
2023-01-03 21:53:44 +0800

    feat: 自定义主题色

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tablet/routes/index.dart';
-import 'package:tablet/style/theme/universal.dart';
+import 'package:tablet/style/theme/color.dart';
 
 void main() {
   runApp(GetMaterialApp(
@@ -9,9 +9,5 @@ void main() {
     initialRoute: '/',
     getPages: appRoutes,
     theme: universal,
-    // theme: ThemeData(
-    //   primaryColor: const Color(0xFFFFB800),
-    //   // primarySwatch: Colors.red,
-    // ),
   ));
 }
